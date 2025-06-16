2d80dc00d51f10ed8a044f7ddf67a19d82398410
Hemanshujc1 <hemanshujc01@gmail.com>
2025-06-16 16:24:57 +0530

    name changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import React, { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import InitialPage from "./components/IntialPage/IntialPage";
 import Navbar from "./components/Navbar/Navbar";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Experience from "./pages/Experience/Experience";
 import Work from "./pages/Work/Work";
+import Topofpage from "./components/Topofpage/Topofpage";
+
 
 function App() {
   const [showInitial, setShowInitial] = useState(true);
-
   return (
     <div className="relative min-h-screen overflow-x-hidden">
       <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:50px_50px]"></div>
@@ -23,7 +23,7 @@ function App() {
       ) : (
         <>
           <Navbar />
-          <ScrollToTop />
+          <Topofpage />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/About" element={<About />} />
